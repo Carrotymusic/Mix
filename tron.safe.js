6c6a426fc536d68facde846bf7d42dfb5932bd98
Taras Shumskyi <tarasshumskyi@mbp-taras.home>
2025-11-28 00:18:23 +0100

    Done

---
+++ tron.safe.js
@@ -0,0 +1,6 @@
+if (window.tronWeb && window.tronWeb.ready) {
+    console.log('Using injected tronWeb');
+  } else {
+    // Bezpieczna inicjalizacja własnego tronWeb tylko jeśli nic nie ma
+    // inicjalizuj tu
+  }
